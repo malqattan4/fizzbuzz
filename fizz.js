f0f5062ed0a3454b38a7cc0b5e02972ce8357a8f
malqattan4 <106008076+malqattan4@users.noreply.github.com>
2022-05-26 13:37:52 +0300

    Add files via upload

---
+++ fizz.js
@@ -0,0 +1,21 @@
+//For each number that is a multiple of 3, print "Fizz"
+//For each number that is a multiple of 5, print "Buzz"
+//For numbers which are a multiple of both 3 and 5, print "FizzBuzz"
+//All other numbers should just print normally
+
+
+
+for (i = 0; i <= 100; i++) {
+    if (i % 3 == 0) {
+        console.log("Fizz");
+    }
+    else if (i % 5 == 0) {
+        console.log("Buzz");
+    }
+    else if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+    }
+    else {
+        console.log(i);
+    }
+}
